51bc158140a12db6360d405cbcd0920630ed7201
qmore <106353165@qq.com>
2015-05-14 09:26:58 +0800

    modify db,main

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,10 +4,9 @@
 return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	
-	'connectionString' => 'mysql:host=localhost;dbname=yii1basic',
+	'connectionString' => 'mysql:host=10.4.12.173;dbname=d6432672e8e64440ea68b82cffbe75f70',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '123456',
+	'username' => 'u4fBwDJ6WYhZI',
+	'password' => 'pdhgItaZ4Bn7y',
 	'charset' => 'utf8',	
 );
