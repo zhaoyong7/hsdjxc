c864e6ffee906a129e23ae91ed3dbc8b65e2d4d1
qmore <106353165@qq.com>
2015-05-14 09:12:43 +0800

    test db_pro

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,10 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="language" content="en">
-
-    <?php echo Yii::app()->request->baseUrl,"<br>";?>
-    <?php echo Yii::app()->request->baseUrl?>
-
+    <?PHP echo DB_HOST?>
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
